18fd033dcf60e4a6da385a7302387fe41959d040
Vladimir-Danilin <121000472+Vladimir-Danilin@users.noreply.github.com>
2022-12-21 13:57:12 +0600

    Session3

---
+++ Sission3/Add_Raz.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Sission3
+{
+    /// <summary>
+    /// Логика взаимодействия для Add_Raz.xaml
+    /// </summary>
+    public partial class Add_Raz : Window
+    {
+        private Разговоры _currentRaz = new Разговоры();
+        public Add_Raz()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Session2Entities.GetContext().Разговоры.Add(_currentRaz);
+        }
+    }
+}
---
+++ Sission3/Razgovori.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sission3
+{
+    /// <summary>
+    /// Логика взаимодействия для Razgovori.xaml
+    /// </summary>
+    public partial class Razgovori : Window
+    {
+        public Razgovori()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DGridRazgovori.ItemsSource = Session2Entities.GetContext().Разговоры.ToList();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Add_Raz ADD = new Add_Raz();
+            ADD.Show();
+        }
+    }
+}
